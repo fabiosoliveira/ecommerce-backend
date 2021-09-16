22478afe84e05f6c677f51a02a7c1a710ef60972
Fábio <fabio.oliveira@poscontrole.com.br>
2021-09-16 20:29:32 -0300

    remove unused methods

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -2,39 +2,20 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { CreateProductDto } from './dto/create-product.dto';
-import { UpdateProductDto } from './dto/update-product.dto';
 import { Product, ProductDocument } from './entities/product.entity';
 
 @Injectable()
 export class ProductsService {
-  constructor(@InjectModel(Product.name) private productModel: Model<ProductDocument>) {}
+  constructor(
+    @InjectModel(Product.name) private productModel: Model<ProductDocument>,
+  ) {}
 
   create(createProductDto: CreateProductDto) {
-    const product = new this.productModel(createProductDto)
-    return product.save()
+    const product = new this.productModel(createProductDto);
+    return product.save();
   }
 
   findAll() {
-    return this.productModel.find()
-  }
-
-  findOne(id: string) {
-    return this.productModel.findById(id)
-  }
-
-  update(id: string, updateProductDto: UpdateProductDto) {
-    return this.productModel.findByIdAndUpdate({
-      _id: id
-    }, {
-      $set: updateProductDto
-    },{
-      new: true
-    })
-  }
-
-  remove(id: string) {
-    return this.productModel.deleteOne({
-      _id: id
-    }).exec()
+    return this.productModel.find();
   }
 }
