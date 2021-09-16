2a1982c039b2dbec439d59e9285da646640de046
Fábio Souza Oliveira <fabio_net1000@hotmail.com>
2021-09-15 23:30:25 -0300

    create product resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
-
+import { MongooseModule } from '@nestjs/mongoose';
+import { ProductsModule } from './products/products.module';
+const password = 'yxffKcSs96MMXxN8'
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(`mongodb+srv://erca:${password}@fabioteste.7iby2.mongodb.net/test`),
+    ProductsModule
+  ],
   controllers: [],
   providers: [],
 })
