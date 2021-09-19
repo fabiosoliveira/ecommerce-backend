8198fbd31819ca5063d62dd89a2b9fa3a9367a3b
Fábio <fabio.oliveira@poscontrole.com.br>
2021-09-19 19:52:16 -0300

    add cache manager

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,24 @@
-import { Module } from '@nestjs/common';
+import { CacheInterceptor, CacheModule, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ProductsModule } from './products/products.module';
 @Module({
   imports: [
+    CacheModule.register({
+      ttl: 60 * 60,
+      max: 10000,
+    }),
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.MONGODB_STRING_CONECTION),
-    ProductsModule
+    ProductsModule,
   ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: CacheInterceptor,
+    },
+  ],
 })
 export class AppModule {}
